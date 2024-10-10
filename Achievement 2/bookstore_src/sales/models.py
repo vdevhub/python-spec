e825bd12058cfc2da8b46780fecc6844dd59a121
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-10-10 09:05:03 +0200

    Define models

--- Achievement 2/bookstore_src/sales/models.py
+++ Achievement 2/bookstore_src/sales/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from books.models import Book
+
 
 # Create your models here.
+class Sale(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveBigIntegerField()
+    price = models.FloatField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            str(self.book.name)
+            + " sold "
+            + str(self.quantity)
+            + " pcs for"
+            + str(self.price)
+            + " on "
+            + str(self.date_created)
+        )
--- Achievement 2/bookstore_src/salespersons/models.py
+++ Achievement 2/bookstore_src/salespersons/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Salesperson(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    bio = models.TextField(default="no bio...")
+
+    def __str__(self):
+        return f"Profile of {self.username}"

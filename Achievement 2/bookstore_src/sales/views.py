59126f33ce083b6e71158f6db657c797f541832e
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-10-19 07:58:43 +0200

    Add records page

--- Achievement 2/bookstore_src/sales/views.py
+++ Achievement 2/bookstore_src/sales/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "sales/home.html")
+
+
+# define function-based view - records(records()
+def records(request):
+    # do nothing, simply display page
+    return render(request, "sales/records.html")

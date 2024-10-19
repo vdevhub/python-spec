59ed17846b1f2f6366f5b9fbb6e84a0d735eb88d
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-10-19 07:36:32 +0200

    Implement login

--- Achievement 2/bookstore_src/bookstore/urls.py
+++ Achievement 2/bookstore_src/bookstore/urls.py
@@ -19,10 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import login_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("sales.urls")),
     path("books/", include("books.urls")),
+    path("login/", login_view, name="login"),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

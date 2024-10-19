71416ceb2ae09f39d35aec8ad466d9cfa48eb45d
vdevhub <158032477+vdevhub@users.noreply.github.com>
2024-10-19 12:31:35 +0200

    Add views protection

--- Achievement 2/bookstore_src/sales/views.py
+++ Achievement 2/bookstore_src/sales/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+# to protect function-based views
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def home(request):
@@ -7,6 +10,8 @@ def home(request):
 
 
 # define function-based view - records(records()
+# keep protected
+@login_required
 def records(request):
     # do nothing, simply display page
     return render(request, "sales/records.html")
